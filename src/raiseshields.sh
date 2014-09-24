23277e3972e08b9aa8d6cacab6f0e30f9d8e5aac
Beau Bouchard <bnb2011@gmail.com>
2014-09-24 13:24:29 -0400

    Update raiseshields.sh

--- src/raiseshields.sh
+++ src/raiseshields.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-#A small script to load a list of IPs into iptables
+# A small script to load a list of IPs into iptables
+# 
 
-IPS_TO_BLOCK="/etc/iptablesv4.list"
-IPTABLES="/sbin/iptables"
-if [ -f $IPS_TO_BLOCK ]; then
-	while read BLOCKED; do
-		$IPTABLES -A INPUT -i eth0 -s $BLOCKED -j DROP
-		$IPTABLES -A OUTPUT -o eth0 -d $BLOCKED -j DROP
-	done < $IPS_TO_BLOCK
-fi
+
+IPS_TO_BLOCK=`cat /etc/iptablesv4.list`
+IPTABLES=`/sbin/iptables`
+CURRENT_RULES=`iptables -L`
+IPTABLES=`whereis iptables | awk '{print $2}'`
+
+for ENTRIES in IPS_TO_BLOCK; 
+	do
+	SUCCESS=0
+	grep "$ENTRIES" "$CURRENT_RULES"
+		if [ ! "$?" = "$SUCCESS" ]; 
+			then 
+			$IPTABLES -I INPUT -i eth0 -s $ENTRIES -j DROP
+			$IPTABLES -I OUTPUT -o eth0 -d $ENTRIES -j DROP
+		fi
+done
