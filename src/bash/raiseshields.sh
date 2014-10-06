e69cd12e837ef98d24b6182181cf2e5a42cf4d85
Beau Bouchard <bnb2011@gmail.com>
2014-10-06 17:50:02 -0400

    Create raiseshields.sh

---
+++ src/bash/raiseshields.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# A small script to load a list of IPs into iptables
+
+IPS_TO_BLOCK=/etc/iptablesv4.list
+
+#checks to see if iptables list exists
+if [ ! -f $IPS_TO_BLOCK ]
+        then
+                echo "Unable to add blocks to IPTABLES because file $IPS_TO_BLOCK is missing"
+                exit
+        fi
+
+CURRENT_RULES=`iptables -nL`
+
+while read entries ;do
+
+        # skip comment lines starting with ; or #
+        case $entries in
+                \#*|\;*)
+                continue
+                ;;
+        esac
+
+        if [[ $CURRENT_RULES =~ $entries ]]
+        then
+                printf "%-20s %20s\n" $entries 'already referenced in iptable - skipping'
+                else
+                # is this CIDR, range or single IP?
+                	#--src-range is not supported in most 1.4.x version of iptables, so i commented this out
+                        #if [[ $entries =~ "-" ]]
+	                #then
+	                        #printf "%-20s %20s %1s %1s\n" 'ADDING RULE:' 'iptables -A INPUT --src-range' $entries '-j DROP'
+	                        #iptables -A INPUT --src-range $entries -j DROP
+	                #else
+	                        
+                        #--CIDR or single
+                        printf "%-20s %20s %1s %1s\n" 'ADDING RULE:' 'iptables -A INPUT -s' $entries '-j DROP'
+                        iptables -A INPUT -s $entries -j DROP
+                        #fi
+                fi
+done < $IPS_TO_BLOCK
